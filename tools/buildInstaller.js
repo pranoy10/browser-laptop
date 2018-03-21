6ea8a0f8d250ad9c8e4da819ac6f4ea0ba54dc41
Pranjal <pranjal@brave.com>
2018-03-21 15:56:27 -0700

    linux bundle

--- tools/buildInstaller.js
+++ tools/buildInstaller.js
@@ -238,7 +238,7 @@ if (isDarwin) {
 
   cmds = [
     // Verify signature of the tor binary and package with installer
-    'mkdir ' + `${appName}-linux-x64/brave/resources/extensions`,
+    'mkdir -p ' + `${appName}-linux-x64/brave/resources/extensions`,
     'curl -o ' + `${appName}-linux-x64/brave/resources/extensions/tor` + ' ' + torURL,
     'curl -o ' + torSigPath + ' ' + torSigURL,
     'gpg --verify ' + torSigPath + ' ' + `${appName}-linux-x64/brave/resources/extensions/tor`,
